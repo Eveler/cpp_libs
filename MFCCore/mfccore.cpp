c9f0c284422a4df300bf756942543b244e7888dc
Savenko <evelerwork@mail.ru>
2012-11-21 17:02:24 +1100

    fixes

--- MFCCore/mfccore.cpp
+++ MFCCore/mfccore.cpp
@@ -10,6 +10,15 @@ int MFCCore::findColumn( QAbstractItemModel *model, const QString &name )
     if ( model->headerData( cIdx, Qt::Horizontal ).toString() == name )
       return cIdx;
 
+  QList<QVariant> vals1 = QList<QVariant>() << 1 << 3 << 5;
+  QList<QVariant> vals2 = QList<QVariant>() << 1 << 2 << 5;
+  QList<QVariant> vals3 = MFCCore::notExists( vals1, vals2 );
+
+  QStringList vals4 = QStringList() << "1" << "3" << "5";
+  QStringList vals5 = QStringList() << "1" << "2" << "5";
+  QStringList vals6 = MFCCore::notExists( vals4, vals5 );
+  vals6 = MFCCore::exists( vals4, vals5 );
+
   return -1;
 }
 
@@ -30,23 +39,3 @@ int MFCCore::findColumn( QAbstractItemModel *model, const QString &name )
 
 //  return result;
 //}
-
-template<class T>
-MFCCORE_EXPORT QList<T> MFCCore::exists( QList<T> list1, QList<T> list2 )
-{
-  QList<T> result = QList<T>();
-  foreach ( T val, list2 )
-    if ( list1.contains( val ) )
-      result << val;
-
-  return result;
-}
-
-template<class T>
-MFCCORE_EXPORT QList<T> MFCCore::notExists( QList<T> list1, QList<T> list2 )
-{
-  QList<T> result = list1;
-  foreach ( T val, list2 ) result.removeOne( val );
-
-  return result;
-}
--- include/mfccore.h
+++ include/mfccore.h
@@ -1,24 +1,33 @@
 #ifndef MFCCORE_H
 #define MFCCORE_H
 
-#if defined(MFCCORE_LIBRARY)
-#  define MFCCORE_EXPORT Q_DECL_EXPORT
-#else
-#  define MFCCORE_EXPORT Q_DECL_IMPORT
-#endif
-
 #include <QAbstractItemModel>
 
-class MFCCore
+namespace MFCCore
 {
-public:
-  static int findColumn( QAbstractItemModel *model, const QString &name );
+static int findColumn( QAbstractItemModel *model, const QString &name );
 //  static QList<QVariant> exists( QList<QVariant> list1, QList<QVariant> list2 );
 //  static QList<QVariant> notExists( QList<QVariant> list1, QList<QVariant> list2 );
-  template<class T>
-  MFCCORE_EXPORT static QList<T> exists( QList<T> list1, QList<T> list2 );
-  template<class T>
-  MFCCORE_EXPORT static QList<T> notExists( QList<T> list1, QList<T> list2 );
-};
+template<class T>
+static QList<T> exists( QList<T> list1, QList<T> list2 )
+{
+  QList<T> result = QList<T>();
+  foreach ( T val, list2 )
+    if ( list1.contains( val ) )
+      result << val;
+
+  return result;
+}
+
+template<class T>
+static QList<T> notExists( QList<T> list1, QList<T> list2 )
+{
+  QList<T> result = list1;
+  foreach ( T val, list2 ) result.removeOne( val );
+
+  return result;
+}
+
+}
 
 #endif // MFCCORE_H
