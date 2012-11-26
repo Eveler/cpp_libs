5b1d2f1e58d3c5a0a0f86d90a2293a71e81b96b8
Savenko <evelerwork@mail.ru>
2012-11-26 17:00:53 +1100

    fix

--- MFCCore/mfccore.cpp
+++ MFCCore/mfccore.cpp
@@ -1,7 +1,5 @@
 #include "mfccore.h"
 
-#include <QStringList>
-
 int MFCCore::findColumn( QAbstractItemModel *model, const QString &name )
 {
   if ( model == NULL ) return -1;
@@ -9,15 +7,6 @@ int MFCCore::findColumn( QAbstractItemModel *model, const QString &name )
   for ( int cIdx = 0; cIdx < model->columnCount(); cIdx++ )
     if ( model->headerData( cIdx, Qt::Horizontal ).toString() == name )
       return cIdx;
-
-  QList<QVariant> vals1 = QList<QVariant>() << 1 << 3 << 5;
-  QList<QVariant> vals2 = QList<QVariant>() << 1 << 2 << 5;
-  QList<QVariant> vals3 = MFCCore::notExists( vals1, vals2 );
-
-  QStringList vals4 = QStringList() << "1" << "3" << "5";
-  QStringList vals5 = QStringList() << "1" << "2" << "5";
-  QStringList vals6 = MFCCore::notExists( vals4, vals5 );
-  vals6 = MFCCore::exists( vals4, vals5 );
 
   return -1;
 }
--- include/mfccore.h
+++ include/mfccore.h
@@ -3,29 +3,31 @@
 
 #include <QAbstractItemModel>
 
-namespace MFCCore
+class MFCCore
 {
-static int findColumn( QAbstractItemModel *model, const QString &name );
-template<class T>
-static QList<T> exists( QList<T> list1, QList<T> list2 )
-{
-  QList<T> result = QList<T>();
-  foreach ( T val, list2 )
-    if ( list1.contains( val ) )
-      result << val;
+public:
+  static int findColumn( QAbstractItemModel *model, const QString &name );
 
-  return result;
-}
+  template<class T>
+  static QList<T> exists( QList<T> list1, QList<T> list2 )
+  {
+    QList<T> result = QList<T>();
+    foreach ( T val, list2 )
+      if ( list1.contains( val ) )
+        result << val;
 
-template<class T>
-static QList<T> notExists( QList<T> list1, QList<T> list2 )
-{
-  QList<T> result = list1;
-  foreach ( T val, list2 ) result.removeOne( val );
+    return result;
+  }
 
-  return result;
-}
+  template<class T>
+  static QList<T> notExists( QList<T> list1, QList<T> list2 )
+  {
+    QList<T> result = list1;
+    foreach ( T val, list2 ) result.removeOne( val );
 
-}
+    return result;
+  }
+
+};
 
 #endif // MFCCORE_H
