ab07290fea772a5d396e5a303844a167122e9ec7
Savenko Mike <evelerwork@mail.ru>
2013-08-12 13:54:17 +1100

    ...

--- __QML__/MQMLPlugins/mqmlplugins_plugin.cpp
+++ __QML__/MQMLPlugins/mqmlplugins_plugin.cpp
@@ -6,6 +6,7 @@
 void MQMLPluginsPlugin::registerTypes(const char *uri)
 {
     // @uri mqmlcomponents
+    Q_ASSERT(uri == QLatin1String("mqmlcomponents"));
     qmlRegisterType<MQMLPlugins>(uri, 1, 0, "MQMLPlugins");
 }
 
--- __QML__/MQMLPlugins/mqmlplugins_plugin.h
+++ __QML__/MQMLPlugins/mqmlplugins_plugin.h
@@ -7,7 +7,7 @@ class MQMLPluginsPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
-    
+
 public:
     void registerTypes(const char *uri);
 };
