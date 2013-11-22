4cbac54067ffe8d73239960b0a85f82b444bc564
Mike <mike@mfcsrv.mfc>
2013-11-22 21:01:52 +1100

    AbstractHtmlReportPlugin fixes

--- include/abstracthtmlreportplugin.h
+++ include/abstracthtmlreportplugin.h
@@ -6,8 +6,6 @@
 #include <QObject>
 #include <QtPlugin>
 
-#define setError(str) set_error(__FILE__,__LINE__,str)
-
 struct HumanInfo
 {
   QString surname;
@@ -43,93 +41,42 @@ class AbstractHtmlReportPlugin: public QObject
 {
   Q_OBJECT
 public:
-  explicit AbstractHtmlReportPlugin(QObject * parent=0):
-    QObject(parent),rep(new HtmlReport(this))
-  {
-    connect(rep,SIGNAL(error(QString)),this,SIGNAL(error(QString)));
-  }
-  ~AbstractHtmlReportPlugin(){delete rep;}
+  explicit AbstractHtmlReportPlugin(QObject * parent=0);
+  ~AbstractHtmlReportPlugin();
 
   /// Возвращает имя отчёта
   virtual QStringList names() const=0;
-  bool select(const QString &name){
-    if(!names().contains(name)) return false;
-    selected = name;
-    return true;
-  }
+  virtual bool select(const QString &name);
   /** Возвращает полное имя файла отчёта, которое сможет обработать
    *\a HtmlReport::load(). См. также \a name()
    */
-  virtual QString fullName() const {return "://"+selected+".html";}
-  virtual bool load(){
-    if(!rep->load(fullName())){
-      setError(tr("Ошибка загрузки шаблона: %1").arg(rep->errorString()));
-      return false;
-    }
-    return true;
-  }
-  virtual HtmlReport *report(){return rep;}
+  virtual QString fullName() const;
+  virtual bool load();
+  virtual HtmlReport *report();
 //  virtual bool setData(const QString &reportName,
 //                       const QList<ClientInfo> &clientList,
 //                       const DeclarInfo &declar,
-//                       const QList<MFCDocumentInfo*> docs=QList<MFCDocumentInfo*>())
-//  {
-//    Q_UNUSED(reportName)
-//    Q_UNUSED(clientList)
-//    Q_UNUSED(declar)
-//    Q_UNUSED(docs)
-//    setError(tr("%1::setData(const QString &, const QList<ClientInfo> &, "
-//                "const DeclarInfo &, const QList<MFCDocumentInfo*> &) "
-//                "не реализовано")
-//             .arg(metaObject()->className()));
-//    return false;
-//  }
+//                       const QList<MFCDocumentInfo*> docs=QList<MFCDocumentInfo*>());
   virtual bool setData(const QList<ClientInfo> &clientList,
-                       const DeclarInfo &declar,
-                       const QList<MFCDocumentInfo*> docs=QList<MFCDocumentInfo*>())
-  {
-    Q_UNUSED(clientList)
-    Q_UNUSED(declar)
-    Q_UNUSED(docs)
-    setError(tr("%1::setData(const QList<ClientInfo> &, const DeclarInfo &, "
-                "const QList<MFCDocumentInfo*> &) не реализовано")
-             .arg(metaObject()->className()));
-    return false;
-  }
+                       const DeclarInfo &declar);
   // для расписки ************************************************************//
   virtual bool setData(
       const QList<ClientInfo> &clientList,
       const DeclarInfo &declar,
       const QList<MFCDocumentInfo*> &docs,
       const QList<MFCDocumentInfo*> docsNProvided=QList<MFCDocumentInfo*>(),
-      const QList<MFCDocumentInfo*> outdocs=QList<MFCDocumentInfo*>())
-  {
-    Q_UNUSED(clientList)
-    Q_UNUSED(declar)
-    Q_UNUSED(docs)
-    Q_UNUSED(docsNProvided)
-    Q_UNUSED(outdocs)
-    setError(tr("%1::setData(const QList<ClientInfo> &, const DeclarInfo &, "
-                "const QList<MFCDocumentInfo*> &, "
-                "const QList<MFCDocumentInfo*>, "
-                "const QList<MFCDocumentInfo*>) не реализовано")
-             .arg(metaObject()->className()));
-    return false;
-  }
+      const QList<MFCDocumentInfo*> outdocs=QList<MFCDocumentInfo*>());
   //************************************************************ для расписки //
   // для согласия ПДн ********************************************************//
-  virtual bool setData(const ClientInfo &client,
-                           const DeclarInfo &declar)
-  {
-    Q_UNUSED(client)
-    Q_UNUSED(declar)
-    setError(tr("%1::setData(const ClientInfo &, const DeclarInfo &) "
-                "не реализовано")
-             .arg(metaObject()->className()));
-    return false;
-  }
+  virtual bool setData(const ClientInfo &client, const DeclarInfo &declar);
   //******************************************************** для согласия ПДн //
-  virtual QString lastError() const{return errStr;}
+  void setclient(const QString &c);
+  void setaddress(const QString &a);
+  void setphone(const QString &p);
+  void setorientir(const QString &a);
+  void setsign(const QString &s);
+  void setdate(const QDate &d);
+  virtual QString lastError() const;
 
 signals:
   void error(QString str);
@@ -139,10 +86,7 @@ protected:
   QString selected;
   QString errStr;
 
-  void set_error(const QString file,const int line,const QString str){
-    errStr=tr("%1 (%2 [%3])").arg(str).arg(file).arg(line);
-    emit error(errStr);
-  }
+  void set_error(const QString file,const int line,const QString str);
 };
 
 #define HtmlReportInterface_iid "org.m.htmlreports.plugins"
